470b049eeac1a74e3fbb245c2751ec8d160a5ebc
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-12-26 17:18:43 -0600

    Cleaning the GameControlsSection component.

--- src/sections/GameControlsSection.tsx
+++ src/sections/GameControlsSection.tsx
@@ -15,22 +15,9 @@ const GameControlsSection = () => {
     <>
       <div className={styles.choiceButtonsContainer}>
         {HandOptions}
-        {/* {
-          optionsContext.options.map((option, index) => (
-            <HandSelector key={`index-${name}`} name={option.name} icon={option.icon} />
-          ))
-        } */}
-        {/* Semi-hardcoded hand selectors...from the context */}
-        {/* <HandSelector name={optionsContext.options[0].name} icon={optionsContext.options[0].icon} />
-        <HandSelector name={optionsContext.options[1].name} icon={optionsContext.options[1].icon} />
-        <HandSelector name={optionsContext.options[2].name} icon={optionsContext.options[2].icon} /> */}
-        {/* Hardcoded hand selectors... */}
-        {/* <HandSelector name="rock" icon={<FaRegHandRock size={60} />} />
-        <HandSelector name="paper" icon={<FaRegHandPaper size={60}  />}/>
-        <HandSelector name="scissors" icon={<FaRegHandScissors size={60}  />} /> */}
       </div>
       <button className={styles.playButton}>
-      Play
+        Play
       </button>
     </>
   );
