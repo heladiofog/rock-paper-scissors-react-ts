fd19e5adc52ed1d07a1038e555851620843b3243
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-12-26 23:16:46 -0600

    Adding handchoice prop and handler ti dispatch the state update event.

--- src/components/HandSelector.tsx
+++ src/components/HandSelector.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
+import { useOptions } from '../context/optionsContext';
+import { OptionActionKind } from '../reducers/scoreReducerTypes';
 import styles from './HandSelector.module.css';
 
 interface Props {
-  name: string,
-  icon: JSX.Element
+  name: string;
+  icon: JSX.Element;
+  handChoiceIndex: number;
 }
 
-const HandSelector: React.FC<Props> = ({ name, icon }) => {
+const HandSelector: React.FC<Props> = ({ name, icon, handChoiceIndex }) => {
+  const [handPressed, setHandPressed] = useState<boolean>(false);
+  const optionsContext = useOptions();
+
+  const { dispatch, state } = optionsContext;
+  const selectedHandIndex = state.playerHand; // previous selected option
+
+  const selectOption = (selectedOption: number) => {
+    dispatch({
+      type: OptionActionKind.UPDATE_PLAYER_CHOICE,
+      payload: selectedOption
+    });
+    setHandPressed(true);
+    // console.log(`The selected option's index is: ${selectedOption}`);
+  };
+
+  console.log({state});
   return (
     <>
-      <button className={styles.choiceButton}>
+      <button
+        className={`${styles.choiceButton} ${
+          handPressed && handChoiceIndex === selectedHandIndex ? styles.activeChoice : ''
+        }`}
+        onClick={() => selectOption(handChoiceIndex)}
+      >
         {name}
         {icon}
       </button>
--- src/sections/GameControlsSection.tsx
+++ src/sections/GameControlsSection.tsx
@@ -1,15 +1,20 @@
 import styles from './GameControlsSection.module.css';
 import HandSelector from '../components/HandSelector';
-// icons
-import { FaRegHandPaper, FaRegHandRock, FaRegHandScissors } from 'react-icons/fa';
 import { useOptions } from '../context/optionsContext';
 
 const GameControlsSection = () => {
   const optionsContext = useOptions();
 
   const HandOptions = optionsContext.options.map((option, index) => (
-    <HandSelector key={`${index}-${option.name}`} name={option.name} icon={option.icon} />
+    <HandSelector 
+      key={`${index}-${option.name}`}
+      name={option.name}
+      icon={option.icon}
+      handChoiceIndex={index}
+    />
   ));
+  // do we have access to state here? :O
+  // console.log(optionsContext);
 
   return (
     <>
