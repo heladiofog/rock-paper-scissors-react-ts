b889f65f9589770206b242344591c3d149d5b6b7
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-12-27 00:05:55 -0600

    Installing and setting up testing tools: @testing-library/re act, @testing-library/jest-dom, @testing-library/user-event, @vitest/coverage-v8, jsdom, vitest. Also adding (setupTests.ts) and updating config files (vite.config.ts).

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// Client types from https://vitejs.dev/guide/features
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
