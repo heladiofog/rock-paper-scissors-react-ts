1b0398ae68be15eee87ed9c38ad09e86a0673249
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-12-27 00:13:16 -0600

    Final vite.config.ts settings for jsdom and other configs.

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    testTimeout: 8000,
+  },
 });
